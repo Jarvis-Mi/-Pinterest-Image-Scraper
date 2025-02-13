af07351f4c74ebd1816c0a5b26896bf9b8497003
user.name like :  "mehdi <user.email.like : 'gw2.fighter@gmail.com>
2025-02-13 04:12:01 -0800

    updata setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="pinterest_scraper",
+    name="jarvis-mi-pinterest-scraper",
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "playwright",
+        'playwright',
     ],
-    entry_points={
-        "console_scripts": [
-            "pinterest-scraper=pinterest_scraper.cli:main"
-        ]
-    },
-    author="نام شما",
-    description="A Pinterest image scraper using Playwright",
+    description="A simple Pinterest image scraper",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Mahdi Ajami",
+    author_email="gw2.fighter@gmail.com",
+    url="https://github.com/Jarvis-Mi/-Pinterest-Image-Scraper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
