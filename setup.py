c81f5f71ad509d751bf6ee068b34890340ccdcf0
user.name like :  "mehdi <user.email.like : 'gw2.fighter@gmail.com>
2025-02-13 05:07:51 -0800

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="auto-pinterest-image-download",
+    name="auto-pi-download",
     version="0.1",
     packages=find_packages(),
     install_requires=[
